1cec3b9ceac9252325f178655f4798eb96d8d118
Dmitriy Fokin <FokinDS1@gmail.com>
2018-04-24 14:02:33 +0300

    add button start. Prog is work, but a lot of work remains to be done

---
+++ Fedorov/Fedorov.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-04-23T18:02:23
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT 		 += widgets serialport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Fedorov
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
---
+++ Fedorov/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtCore/QtGlobal>
+#include <QWidget>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QProgressBar>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QComboBox>
+
+
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    QPushButton *button_start;
+    QPushButton *button_stop;
+    QLabel *label_distance;
+    QLabel *label_cm;
+    QLabel *label_serialName;
+    QComboBox *comboBox_serialName;
+    QSerialPort *serial;
+
+    void FillComboBoxWithSerialPortNames();
+    void initializeSerialPort();
+
+    QProgressBar *distanceBar; // may be delete
+
+
+private slots:
+    void Slot_readFromSerialPort();
+    void Slot_distanceProgramSTART();
+    void Slot_distanceProgramSTOP();
+};
+
+#endif // MAINWINDOW_H
